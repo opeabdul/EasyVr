d9a03b13d6f6ddcda779aad86598f21ff7c2e9b6
Opeyemi <opdort24@gmail.com>
2020-07-02 18:26:40 +0100

    description screens design complete

--- app/src/main/java/com/example/easyvr/TourGallery.kt
+++ app/src/main/java/com/example/easyvr/TourGallery.kt
@@ -7,25 +7,26 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.example.easyvr.databinding.TourGalleryBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class TourGallery : Fragment() {
-
+    lateinit var mBinding: TourGalleryBinding
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.tour_gallery, container, false)
+        mBinding = TourGalleryBinding.inflate(inflater)
+        return mBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        mBinding.tuneImageView.setOnClickListener {
+            findNavController().navigate(R.id.tour_gallery_to_add_description)
         }
     }
 }
